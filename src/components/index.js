371cc4fcd9f6daddb4354ee1fb059fc7d813a19f
Aditya1016 <iamaditya.sinha16@gmail.com>
2024-10-18 07:45:03 +0530

    Mega Blog

---
+++ src/components/index.js
@@ -0,0 +1,40 @@
+import Header from "./header/Header";
+import Footer from "./footer/Footer";
+import Container from "./container/Container";
+import Logo from "./Logo";
+import LogoutBtn from "./header/LogoutBtn";
+import Button from "./Button"
+import RTE from "./RTE"
+import Signup from "./SignUp";
+import Login from "./Login"
+import Post from "./pages/Post"
+import PostForm from "./post-form/PostForm"
+import PostCard from "./PostCards"
+import AuthLayout from './AuthLayout'
+import Select from './Select'
+import Input from './Input'
+import AllPosts from './pages/AllPosts'
+import AddPosts from './pages/AddPost'
+import EditPosts from './pages/EditPost'
+import Home from './pages/Home'
+export {
+    Header,
+    Footer,
+    Container, 
+    Logo,
+    LogoutBtn,
+    Button,
+    RTE,
+    Signup,
+    Login,
+    Post,
+    PostForm,
+    PostCard,
+    AuthLayout,
+    Select,
+    Input,
+    AllPosts,
+    AddPosts,
+    EditPosts,
+    Home
+}
